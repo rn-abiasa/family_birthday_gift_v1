69627d989b6788b2d2dba24b2487bfe48beae88d
abiasa <rnabiasa@gmail.com>
2026-09-12 14:47:44 +0700

    Create carousel for memories section

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import "@fontsource/pacifico";
+import "@fontsource-variable/inter";
+import "@fontsource/oooh-baby";
 import "./index.css";
 import Cover from "./pages/cover.jsx";
 
